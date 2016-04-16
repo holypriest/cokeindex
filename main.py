979957f651e5067a89e8c628ad52dd73434727fd
holypriest <chemistoid@gmail.com>
2016-04-16 16:24:57 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+data = {
+    'pda' : {
+        'selector': '#productForm > div.product-control__price.product-control__container > span.value.inline--middle',
+        'url': 'http://www.paodeacucar.com.br/produto/116609',
+    },
+    'extra' : {
+        'selector': '#prod_116609 > p',
+        'url': 'http://www.deliveryextra.com.br/produto/116609/refrigerante-coca-cola-lata-350ml',
+    }
+}
+def main():
+
+    selector, url = get_data('extra')
+
+    content = fetch(url)
+    price = get_price(content, selector)
+
+    print(price)
+
+def fetch(url):
+
+    r = requests.get(url)
+    return r.text
+
+def get_price(text, selector):
+
+    soup = BeautifulSoup(text, 'html.parser')
+    result = soup.select(selector)
+
+    return float(result[0].text.replace(',', '.'))
+
+def get_data(key):
+    return (data[key]['selector'], data[key]['url'])
+
+def fix_price(price_str):
+    price = re.sub(r',', '.', price_str)
+    return float(re.sub(r'[^0-9\.]', '', price))
+
+
+if __name__ == '__main__':
+    main()
