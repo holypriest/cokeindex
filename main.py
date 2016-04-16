94c8ac9b1a3e179122fc20efe66f684e89d99da4
Fernando Freitas Alves <ffreitasalves@gmail.com>
2016-04-16 16:49:41 -0300

    added currency

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ data = {
         'selector': '#productForm > div.product-control__price.product-control__container > span.value.inline--middle',
         'url': 'http://www.paodeacucar.com.br/produto/116609',
         'country': 'brazil',
+        'currency': 'BRL',
     },
     'extra' : {
         'selector': '#prod_116609 > p',
         'url': 'http://www.deliveryextra.com.br/produto/116609/refrigerante-coca-cola-lata-350ml',
         'country': 'brazil',
+        'currency': 'BRL',
     },
     'ardis': {
         'selector': '#main_container > section > div > div.span5 > div > div.price_block.line-dashed > p > span',
         'url': 'http://www.ardis.dz/arproduit_detail.php?Id_Prod=10000086',
-        'country': 'algeria',
+        'country': 'Algeria',
+        'currency': 'DZD',
     },
 }
 
